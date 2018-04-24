955fefcc92ded5d25e0027c4198878bdfa23a9fb
David Borrego <dborrego@atsistemas.com>
2018-04-25 01:10:24 +0200

    transaltion

--- src/main/java/com/dbg/jenkinspipelinebase/dto/TranslationRequestDTO.java
+++ src/main/java/com/dbg/jenkinspipelinebase/dto/TranslationRequestDTO.java
@@ -2,9 +2,11 @@ package com.dbg.jenkinspipelinebase.dto;
 
 import java.io.Serializable;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class TranslationRequestDTO implements Serializable {
 
 	private static final long serialVersionUID = 5666862072082525898L;
--- src/main/java/com/dbg/jenkinspipelinebase/view/AppUI.java
+++ src/main/java/com/dbg/jenkinspipelinebase/view/AppUI.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dbg.jenkinspipelinebase.dao.TranslateDao;
+import com.dbg.jenkinspipelinebase.dto.TranslationDTO;
+import com.dbg.jenkinspipelinebase.dto.TranslationRequestDTO;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
@@ -19,6 +21,7 @@ public class AppUI extends UI {
 
 	private static final NativeSelect<String> SELECT = new NativeSelect<>();
 	private static final TextField T_FIELD = new TextField();
+	private static final TextField TRANSLATION = new TextField();
 
 	private static final long serialVersionUID = 8283544339996644259L;
 
@@ -30,13 +33,22 @@ public class AppUI extends UI {
 		final VerticalLayout vLayout = new VerticalLayout();
 		final HorizontalLayout hLayout = new HorizontalLayout();
 		SELECT.setItems(Arrays.asList("de", "en"));
-		SELECT.setSelectedItem("1");
+		SELECT.setSelectedItem("2");
 		hLayout.addComponent(SELECT);
 		hLayout.addComponent(T_FIELD);
 		final Button btn = new Button("Traducir");
 		hLayout.addComponent(btn);
+		btn.addClickListener(e -> {
+			final TranslationRequestDTO rq = TranslationRequestDTO.builder().language(SELECT.getValue())
+					.text(T_FIELD.getValue()).build();
+			final TranslationDTO translate = dao.translate(rq);
+			TRANSLATION.setValue(translate.getText());
+		});
 
 		vLayout.addComponent(hLayout);
+
+		TRANSLATION.setEnabled(false);
+		vLayout.addComponent(TRANSLATION);
 
 		setContent(vLayout);
 	}
