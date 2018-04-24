336207c88d2a65b4630536fed2220bcf59a580d2
David Borrego <dborrego@atsistemas.com>
2018-04-25 01:02:41 +0200

    select

--- src/main/java/com/dbg/jenkinspipelinebase/view/AppUI.java
+++ src/main/java/com/dbg/jenkinspipelinebase/view/AppUI.java
@@ -1,5 +1,7 @@
 package com.dbg.jenkinspipelinebase.view;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dbg.jenkinspipelinebase.dao.TranslateDao;
@@ -7,12 +9,16 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
 public class AppUI extends UI {
+
+	private static final NativeSelect<String> SELECT = new NativeSelect<>("", Arrays.asList("de", "en"));
+	private static final TextField T_FIELD = new TextField();
 
 	private static final long serialVersionUID = 8283544339996644259L;
 
@@ -23,8 +29,8 @@ public class AppUI extends UI {
 	protected void init(VaadinRequest request) {
 		final VerticalLayout vLayout = new VerticalLayout();
 		final HorizontalLayout hLayout = new HorizontalLayout();
-		final TextField tField = new TextField();
-		hLayout.addComponent(tField);
+		hLayout.addComponent(SELECT);
+		hLayout.addComponent(T_FIELD);
 		final Button btn = new Button("Traducir");
 		hLayout.addComponent(btn);
 
